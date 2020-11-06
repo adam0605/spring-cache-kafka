8e021e477b86da62a7c2d120c2e93387d05c6300
서동우Dongwoo Seo <dongwoo.seo@bestpinglobal.com>
2020-11-06 13:20:07 +0900

    init-project

---
+++ src/main/java/com/example/springcachekafka/SpringCacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheKafkaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCacheKafkaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springcachekafka/SpringCacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
